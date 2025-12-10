15ed317f626db2e0788ffe3e1c5a38f13de9b7b3
mbboy010 <mbboy010@gmail.com>
2025-12-10 23:39:23 +0100

    Initial commit

---
+++ src/components/collaboration/Collaboration.tsx
@@ -0,0 +1,55 @@
+"use client"
+
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { setIsAside } from "../redux/slicer/AsideCheck";
+import { setPosit } from "../redux/slicer/posit";
+import { setOpacity } from "../redux/slicer/opacity";
+import Loading from "../loading/Loading";
+
+import CollaborationCom from "./CollaborationCom"
+
+
+
+export default function Collaboration() {
+  
+  
+  const dispatch = useDispatch();
+  
+  // FIX: Start loading as true so the user sees the loader first
+  const [loading, setLoading] = useState<boolean>(true);
+  
+  useEffect(() => {
+    // Dispatch initial Redux states
+    dispatch(setOpacity("0"));
+    dispatch(setPosit("-79vw"));
+
+    const timer = setTimeout(() => {
+      dispatch(setIsAside(false));
+      // FIX: Update local state to stop loading after the timeout
+      setLoading(false); 
+    }, 3000);
+
+    // FIX: Cleanup timer if component unmounts
+    return () => clearTimeout(timer);
+  }, [dispatch]);
+  
+  
+  
+  return (
+    <div className="relative">
+        {
+          loading ? (
+            <Loading />
+          ) : (
+            <>
+              {/* FIX: Removed 'none' class which might obscure content unexpectedly */}
+              <div className="relative z-10">
+                <CollaborationCom />
+              </div>
+            </>
+          )
+        }
+    </div>
+  )
+}
